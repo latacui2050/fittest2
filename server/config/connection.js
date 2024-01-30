b8cb72241531ac26ee6a5fdc89d3065a9e260166
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-01-30 04:44:16 -0800

    changed mongodb uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ellencui15:bXyHZOo3MPjCsoSQ@cluster0.6y62e01.mongodb.net/firstdb?retryWrites=true&w=majority/');
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.jtey2ed.mongodb.net/');
 module.exports = mongoose.connection;
 
 // const sequelize = new Sequelize(

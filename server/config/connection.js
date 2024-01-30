ae69d80f7575037bd612793682e7299fac9931dd
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-01-29 20:25:50 -0800

    cd ../client

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.jtey2ed.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ellencui15:bXyHZOo3MPjCsoSQ@cluster0.6y62e01.mongodb.net/firstdb?retryWrites=true&w=majority/');
 
 module.exports = mongoose.connection;
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );

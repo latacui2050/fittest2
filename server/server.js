7658410e92ebb766da4aca9cf817abc3ccb6d29b
Wenmo Cui <wenmo_cui@berkeley.edu>
2024-01-29 21:22:14 -0800

    hi

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   if (process.env.NODE_ENV === 'production') {
+    console.log('hi')
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
